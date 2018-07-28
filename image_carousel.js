1c07fc2739cb25e90c796c4c43c23e95e671eb46
Chad Chaney <cchane4@gmail.com>
2018-07-27 23:24:23 -0500

    finished up with added comments

--- image_carousel.js
+++ image_carousel.js
@@ -1,19 +1,42 @@
+// create the carousel
 let carousel = document.getElementsByClassName('image_carousel'); 
-
+// for each index of the carousel 
 [].forEach.call(carousel, function (c) {
     let next = c.getElementsByClassName('next')[0],
          previous = c.getElementsByClassName('previous')[0],
-        bubbles = c.getElementsByClassName('bubbles')[0],
+        bubblesContainer = c.getElementsByClassName('bubbles')[0],
         inner = c.getElementsByClassName('inner')[0],
         imgs = inner.getElementsByTagName('img'); 
         currentImageIndex = 0;
         width = 308; 
+        bubbles = []; 
+// creates the four bubbles seen at the bottom of the carousel
+    for (let i = 0; i < imgs.length; i++){ 
+        let b = document.createElement('span');
+        b.classList.add('bubble');
+        bubblesContainer.appendChild(b); 
+        bubbles.push(b); 
+   // if the cursor clicks on any given image referenced by the bubble, the carousel slides to that image      
+        b.addEventListener('click', function(){ 
+            currentImageIndex = i; 
+            switchImg(); 
+        })
 
+    }
+// functionality for switching the image and the current image corresponds with the only bubble that is white and opaque
+// so if the image is image #1, then bubble number 1 will be white
    function switchImg () { 
         inner.style.left = -width * currentImageIndex + 'px'; 
+        bubbles.forEach( function(b, i){ 
+            if (i === currentImageIndex){ 
+                b.classList.add('active'); 
+            } else{ 
+                b.classList.remove('active'); 
+            }
+        });
     }
 
-
+// each click on the "next" element pushes to the next image
     next.addEventListener("click", function () { 
         currentImageIndex++; 
 
@@ -23,7 +46,7 @@ let carousel = document.getElementsByClassName('image_carousel');
         switchImg(); 
 
     });
- 
+ //each click on the "previous" element slides back to the preceding image
     previous.addEventListener("click", function () { 
          currentImageIndex--; 
 
